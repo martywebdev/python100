19234f69eca23c9f969c75f4631f2acc2f0b7531
martyumlas <martyumlas@gmail.com>
2025-04-09 19:37:35 +0800

    printing

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,2 +1,17 @@
 # Write your code below this line 👇
-print('Idiot admires complexityasdfasdfasdfasdfasdf')
+
+while True:
+    user_input = input("Are you an idiot? (yes/no): ").strip().lower()
+    if user_input in ['yes', 'y', 'true', '1']:
+        is_idiot = True
+        break
+    elif user_input in ['no', 'n', 'false', '0']:
+        is_idiot = False
+        break
+    else:
+        print("Invalid input. Please answer yes or no.")
+
+if is_idiot:
+    print("Idiot admires complexity")
+else:
+    print("Genius admires simplicity")
