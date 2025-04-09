43e7d65f55272205f593afd04e9117c805b28b3d
martyumlas <martyumlas@gmail.com>
2025-04-09 13:57:29 +0800

    initial commit

---
+++ Day 1/task/solution.py
@@ -0,0 +1,10 @@
+# print('This is a newline\nDont forget!')
+# str1 = 'hello'
+# str2 = 'world'
+#
+# print('This is concatenation !' + str1 + ' ' + str2)
+# print(f'This is string literals! {str1} {str2}')
+
+name = input("What is your name? ")
+print(f"hello {name}")
+print("ho ho ho " + input('test'))
